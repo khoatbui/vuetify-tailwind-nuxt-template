3386b384b87559927b4779f8b6b441e368039ea0
khoatbui <khoatbui.dev@gmail.com>
2020-01-30 19:57:27 +0700

    Them v-calendar,finish test

---
+++ components/controls/InlineDateRange.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="inline-daterange">
+    <vc-date-picker
+      :mode="mode"
+      v-model="selectedDate"
+      :is-inline="true"
+      :is-expanded="true"
+      columns="1"
+      rows="12"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'InlineDateRange',
+  data() {
+    return {
+      // Data used by the date picker
+      mode: 'range',
+      selectedDate: null
+    }
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '@/plugins/v-calendar', ssr: false }],
   /*
    ** Nuxt.js dev-modules
    */
